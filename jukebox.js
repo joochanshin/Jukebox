19828341a8c544fa5aabdd6623acdc2242b70c76
JooChan <14jshin@gmail.com>
2018-01-22 16:56:12 -0500

    Added buttons

--- jukebox.js
+++ jukebox.js
@@ -4,27 +4,35 @@ function __hide__ () {
 	document.getElementById("player").style.display = "none";
 }
 
+var song = document.getElementById("player");
+
 var Jukebox = {
 	playlist: [],
 	play: function(){	//	plays song
 		bool = true;
+		song.play();
 	},
-	stop: function(){	//	stops song
+	pause: function(){	//	pauses song
 		bool = false;
+		song.pause();
 	},
-	load: function(){
-		//	loads music
+	load: function(){	//	loads music
+		var new_song = "audio/" + prompt("Enter song name") + ".mp3";
+		this.playlist.push(new_song);
+		console.log(new_song);
 	},
-	volUp: function(song){	//	increases volume
-		song.volume += .1;
+	volUp: function(){	//	increases volume
+		if(song.volume < 1)
+			song.volume += .1;
 	},
 	volDown: function(){	//	Decreases volue
-		song.volume -= .1;
+		if(song.volume > 0)
+			song.volume -= .1;
 	},
 	random: function(){	//	randomizes array
 		var currentIndex = this.playlist.length, temporaryValue, randomIndex;
 
-		while (0 !== currentIndex) {.
+		while (0 !== currentIndex) {
 		    randomIndex = Math.floor(Math.random() * currentIndex);
 		    currentIndex -= 1;
 
@@ -35,10 +43,12 @@ var Jukebox = {
 
 		  return this.playlist;
 	},
-	__add__: function(song){
+	__add__: function(song){	//	adds song from songs to playlist
 		this.playlist.push(song);
 		return "Song added";
 	}
 };
+
+//Jukebox.load();
 
 
