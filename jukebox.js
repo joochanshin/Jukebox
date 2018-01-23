c8616501862bfa1e3e2495ca20e9156604f2703f
JooChan <14jshin@gmail.com>
2018-01-23 14:12:34 -0500

    finished jukebox/boombox/stereo

--- jukebox.js
+++ jukebox.js
@@ -7,12 +7,12 @@ function __hide__ () {
 
 var song = document.getElementById("player");
 
-song.addEventListener('ended',function(){
-	song.setAttribute("src", Jukebox.playlist[index]);
+song.addEventListener("ended",function(){
 	index++;
 	if(index === Jukebox.playlist.length-1){
 		index = 0;
 	}
+	song.setAttribute("src", Jukebox.playlist[index]);
 	document.getElementById("current").innerHTML = Jukebox.playlist[index];
 });
 
@@ -21,7 +21,7 @@ var smlines = document.getElementsByClassName("smlines");
 var speaker = document.getElementsByClassName("speaker");
 
 var Jukebox = {
-	playlist: ["audio/birds.mp3", "audio/next.mp3"],//next.mp3 is to check if it works
+	playlist: ["audio/birds.mp3", "audio/humble.mp3", "audio/dead-wrong.mp3", "audio/go-to-sleep.mp3", "audio/hail-mary.mp3"],//next.mp3 is to check if it works
 	play: function(){	//	plays song
 		bool = true;
 		song.play();
@@ -31,6 +31,7 @@ var Jukebox = {
 		}
 		speaker[0].style.animation = "pump .5s ease-in infinite";
 		speaker[1].style.animation = "pump .5s ease-in infinite";
+		gradient(bool);
 
 	},
 	pause: function(){	//	pauses song
@@ -42,6 +43,7 @@ var Jukebox = {
 		}
 		speaker[0].style.animation = "none";
 		speaker[1].style.animation = "none";
+		gradient(bool);
 	},
 	next: function(){
 		if(index === Jukebox.playlist.length-1){
@@ -88,6 +90,14 @@ var Jukebox = {
 		    this.playlist[randomIndex] = temporaryValue;
 		}
 		console.log("Randomized");
+		if(index === this.playlist.length-1){
+			index = 0;
+		}
+		else{
+			index++;
+		}
+		song.setAttribute("src", Jukebox.playlist[index]);
+		document.getElementById("current").innerHTML = this.playlist[index];
 		return this.playlist;
 	},
 	__add__: function(song){	//	adds song from songs to playlist
@@ -98,6 +108,34 @@ var Jukebox = {
 
 //Jukebox.load();
 document.getElementById("current").innerHTML = Jukebox.playlist[index];
-// setInterval(, 30000);
+
+function gradient(bool){
+	var body = document.getElementById("body");
+	var _1 = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+	var _2 = '#'+(Math.random()*0xFFFFFE<<0).toString(16);
+
+	if(bool){
+		body.style.background = "linear-gradient(-45deg,"+_1+","+_2+")";
+		body.style.backgroundSize = "400% 1000%";
+		body.style.animation = "Gradient 1s ease infinite"
+	}
+	else{
+		body.style.background = "white";
+		body.style.animation = "none";
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
  
